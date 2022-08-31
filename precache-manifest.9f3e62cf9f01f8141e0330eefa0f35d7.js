ec120945dd46254c5a8ecd312edad912f2fa2200
CI <CI@example.com>
2022-08-31 01:35:18 +0000

    deploy

---
+++ precache-manifest.9f3e62cf9f01f8141e0330eefa0f35d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8a9ae7d572d8ec933a",
+    "url": "/Dispatch/css/app.eb36d174.css"
+  },
+  {
+    "revision": "c403317da05ca709d933af00e1e52d5b",
+    "url": "/Dispatch/favicon.png"
+  },
+  {
+    "revision": "f221dd2122fb626e60e315dae84ffc3f",
+    "url": "/Dispatch/img/dispatchLogo.f221dd21.svg"
+  },
+  {
+    "revision": "de350a863b453bf8e64bef98d550a472",
+    "url": "/Dispatch/index.html"
+  },
+  {
+    "revision": "cb538bbe39eb63fdcef6",
+    "url": "/Dispatch/js/about.4de025a2.js"
+  },
+  {
+    "revision": "ad8a9ae7d572d8ec933a",
+    "url": "/Dispatch/js/app.9b37992b.js"
+  },
+  {
+    "revision": "6ebc354a3e03dbadc62f",
+    "url": "/Dispatch/js/chunk-vendors.f53acfe4.js"
+  },
+  {
+    "revision": "318a5fb7ab753e4cd2d3e938e36ea87f",
+    "url": "/Dispatch/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Dispatch/robots.txt"
+  }
+]);
